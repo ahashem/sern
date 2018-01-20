05f9cbba8ecd46a3074b7baa0531525a0fcf043c
Ahmed-Hashem <me@ahashem.me>
2018-01-20 11:49:10 +0200

    tests to fix build

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+/* global describe, it, before, after */
 // Add promise support if this does not exist natively.
 if (!global.Promise) {
   global.Promise = require('q');
@@ -7,18 +8,18 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiThings = require('chai-things');
 const {assert, expect, request, should} = chai;
-const config = require('../project.config');
+// const config = require('../project.config');
 const server = require('../index');
 chai.use(chaiHttp);
 chai.use(chaiThings);
 
 
 describe('Node/Express Server', () => {
-  let app;
+  const app = chai.request(server);
   // const host = `http://${config.server_uri}:${config.server_port}`;
 
   before(done => {
-    app = chai.request(server);
+    // Additional `app` configurations before all Tests
     done();
   });
 
